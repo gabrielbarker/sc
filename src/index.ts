590aa2d439c9d51c91560c730f348494d65c3d97
Gabriel Barker <gabrielbarker@gmail.com>
2022-09-23 11:42:55 +0100

    chore: move case related logic to separate dir

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import App from "./main/App";
-import TextCase from "./main/TextCase";
+import App from "./main/case/App";
+import TextCase from "./main/case/TextCase";
 
 const app = new App();
 console.log(app.swapCase("hello", TextCase.Upper));
