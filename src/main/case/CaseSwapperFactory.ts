3185ac70d28bbd0018e10b92b8e1fd0da9775846
Gabriel Barker <gabrielbarker@gmail.com>
2022-09-23 15:39:57 +0100

    feat: support kebab case in case swapper factory

--- src/main/case/CaseSwapperFactory.ts
+++ src/main/case/CaseSwapperFactory.ts
@@ -6,6 +6,7 @@ import TitleCaseSwapper from "./TitleCaseSwapper";
 import CamelCaseSwapper from "./CamelCaseSwapper";
 import ConstantCaseSwapper from "./ConstantCaseSwapper";
 import DotCaseSwapper from "./DotCaseSwapper";
+import KebabCaseSwapper from "./KebabCaseSwapper";
 
 class CaseSwapperFactory {
   private static CASE_SWAPPERS_BY_TEXT_CASE = {
@@ -15,6 +16,7 @@ class CaseSwapperFactory {
     [TextCase.Camel]: new CamelCaseSwapper(),
     [TextCase.Constant]: new ConstantCaseSwapper(),
     [TextCase.Dot]: new DotCaseSwapper(),
+    [TextCase.Kebab]: new KebabCaseSwapper(),
   };
 
   public getCaseSwapper(textCase: TextCase): CaseSwapper {
