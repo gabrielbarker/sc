80a5f1c3ed40c452abb099dc65f6b4cbdd94e7ab
Gabriel Barker <gabrielbarker@gmail.com>
2022-09-23 15:04:49 +0100

    feat: add comments for each text case

--- src/main/case/TextCase.ts
+++ src/main/case/TextCase.ts
@@ -1,6 +1,18 @@
 enum TextCase {
   Upper,
   Lower,
+  // Camel,
+  // Constant,
+  // Dot,
+  // Kebab,
+  // LowerFirst,
+  // Pascal,
+  // Path,
+  // Sentence,
+  // Snake,
+  // Swap,
+  // Title,
+  // UpperFirst,
 }
 
 export default TextCase;
