f0e16d47e7215d91a02803a7d4b0e34c1b26c1a7
Gabriel Barker <gabrielbarker@gmail.com>
2022-09-23 15:25:00 +0100

    refactor: change app to display data better

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import App from "./main/App";
 import { TextCase } from "./main/case/";
 
-const app = new App();
-const text = "hElLo";
-console.log(text);
-console.log(app.swapCase(text, TextCase.Upper));
-console.log(app.swapCase(text, TextCase.Lower));
-console.log(app.swapCase(text, TextCase.Title));
-console.log(app.swapCase(text, TextCase.Camel));
+print("hElLo");
+print("A simple test string");
+print("camelCaseString");
+print("kebab-case-string");
+
+function print(text: string): void {
+  const app = new App();
+  console.log(text);
+  console.log(`Upper   : ${app.swapCase(text, TextCase.Upper)}`);
+  console.log(`Lower   : ${app.swapCase(text, TextCase.Lower)}`);
+  console.log(`Title   : ${app.swapCase(text, TextCase.Title)}`);
+  console.log(`Camel   : ${app.swapCase(text, TextCase.Camel)}`);
+  console.log(`Constant: ${app.swapCase(text, TextCase.Constant)}`);
+  console.log();
+}
