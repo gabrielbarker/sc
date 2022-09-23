83931151f14bf569510fd548ba910f3c79050658
Gabriel Barker <gabrielbarker@gmail.com>
2022-09-23 15:28:43 +0100

    feat: support dot case in case swapper factory

--- src/main/case/CaseSwapperFactory.ts
+++ src/main/case/CaseSwapperFactory.ts
@@ -5,6 +5,7 @@ import LowercaseSwapper from "./LowercaseSwapper";
 import TitleCaseSwapper from "./TitleCaseSwapper";
 import CamelCaseSwapper from "./CamelCaseSwapper";
 import ConstantCaseSwapper from "./ConstantCaseSwapper";
+import DotCaseSwapper from "./DotCaseSwapper";
 
 class CaseSwapperFactory {
   private static CASE_SWAPPERS_BY_TEXT_CASE = {
@@ -13,6 +14,7 @@ class CaseSwapperFactory {
     [TextCase.Title]: new TitleCaseSwapper(),
     [TextCase.Camel]: new CamelCaseSwapper(),
     [TextCase.Constant]: new ConstantCaseSwapper(),
+    [TextCase.Dot]: new DotCaseSwapper(),
   };
 
   public getCaseSwapper(textCase: TextCase): CaseSwapper {
