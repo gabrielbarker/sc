869cf59e4bde2c706ed6a6affb953709a7031944
Gabriel Barker <gabrielbarker@gmail.com>
2022-11-17 13:04:26 +0000

    feat: add entry point for cli

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
-import App from "./main/App";
-import { TextCase } from "./main/case/";
+import CLI from "./main/cli";
 
-print("hElLo");
-print("A simple test string");
-print("camelCaseString");
-print("kebab-case-string");
-
-function print(text: string): void {
-  const app = new App();
-  console.log(text);
-  console.log(`Upper   : ${app.swapCase(text, TextCase.Upper)}`);
-  console.log(`Lower   : ${app.swapCase(text, TextCase.Lower)}`);
-  console.log(`Title   : ${app.swapCase(text, TextCase.Title)}`);
-  console.log(`Camel   : ${app.swapCase(text, TextCase.Camel)}`);
-  console.log(`Constant: ${app.swapCase(text, TextCase.Constant)}`);
-  console.log(`Dot     : ${app.swapCase(text, TextCase.Dot)}`);
-  console.log(`Kebab   : ${app.swapCase(text, TextCase.Kebab)}`);
-  console.log(`Pascal  : ${app.swapCase(text, TextCase.Pascal)}`);
-  console.log(`Path    : ${app.swapCase(text, TextCase.Path)}`);
-  console.log(`Sentence: ${app.swapCase(text, TextCase.Sentence)}`);
-  console.log(`Snake   : ${app.swapCase(text, TextCase.Snake)}`);
-  console.log(`Invert  : ${app.swapCase(text, TextCase.Invert)}`);
-  console.log();
+export function main() {
+  new CLI().run(process.argv);
 }
